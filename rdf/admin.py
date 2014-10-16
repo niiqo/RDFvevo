949f3d39564ebfecb86cbcfcb16917a310724be7
Luciano Castillo <lucho.castillo97@gmail.com>
2014-10-16 08:41:13 -0300

    Modelos y relaciones hecho

--- rdf/admin.py
+++ rdf/admin.py
@@ -1,12 +1,84 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
-from rdf.models import Preceptor, Curso
+from django import forms
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm
+from rdf.models import Preceptor, Curso, Observacion, Inasistencia, Alumno
 
 
-#class PreceptorAdmin(admin.ModelAdmin):
-    # Informacion que se muestra en la lista
-    #list_display = ('username', 'password')
-    # Busqueda por titulo
-    #search_fields = ['username']
+class UserForm (UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username')
 
-admin.site.register(Preceptor)#, PreceptorAdmin)
+class UserAdmin(admin.ModelAdmin):
+    form = UserForm
+
+admin.site.register(Observacion)
+admin.site.register(Inasistencia)
+admin.site.register(Preceptor, UserAdmin)
+admin.site.register(Alumno, UserAdmin)
 admin.site.register(Curso)
+
+#admin.site.unregister(User)
+#admin.site.unregister(Group)
+
+p = [{'username': 'claudio',
+      'first_name': 'Claudio',
+      'last_name': 'Cavestri',
+      'email': 'claudito@gmail.com',
+      },
+      {'username': 'poli',
+      'first_name': 'Pablo',
+      'last_name': 'Palavecino',
+      'email': 'polito@gmail.com',
+      },
+      {'username': 'juan',
+      'first_name': 'Juan',
+      'last_name': 'Portis',
+      'email': 'donbosco@gmail.com',
+      }]
+
+a = [{'username': 'lucho',
+      'first_name': 'Luciano',
+      'last_name': 'Castillo',
+      'email': 'claudito@gmail.com',
+      },
+      {'username': 'arabe',
+      'first_name': 'Augusto',
+      'last_name': 'Jair',
+      'email': 'polito@gmail.com',
+      },
+      {'username': 'many',
+      'first_name': 'Nicolas',
+      'last_name': 'Ordoñez',
+      'email': 'donbosco@gmail.com',
+      }]
+
+for i in range(3):
+    username = p[i]['username']
+    first_name = p[i]['first_name']
+    last_name = p[i]['last_name']
+    email = p[i]['email']
+    print username
+    pre = Preceptor(username=username, first_name=first_name, last_name=last_name, email=email)
+    pre.save()
+
+    username = a[i]['username']
+    first_name = a[i]['first_name']
+    last_name = a[i]['last_name']
+    email = a[i]['email']
+
+    print username
+    al = Alumno(username=username, first_name=first_name, last_name=last_name, email=email)
+    al.save()
+
+
+
+
+
+
+
+
+
--- rdf/models.py
+++ rdf/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
-class Curso(models.Model):    
+class Curso(models.Model):
     # Atributos de la clase
     numero = models.IntegerField("Curso",max_length=1)
     division = models.CharField("Division", max_length=1)
@@ -15,7 +15,7 @@ class Curso(models.Model):
     # Metodos de la clase
 
     def __unicode__(self):
-        curso = str(self.numero)+u" "+str(self.division)+" "+str(self.anio)
+        curso = str(self.numero)+" \""+str(self.division)+"\" - "+str(self.anio)
         return curso
 
     def getDiasDeClase(self):
@@ -63,30 +63,68 @@ class Curso(models.Model):
         porcentajeDeAsistencia = (asistencias / (alumnos * diasDeClase))
         return porcentajeDeAsistencia
 
-class Preceptor(models.Model):
-    preceptor = models.ManyToManyField(User)#, unique=True)
-    
-    # nombre de usuario
-    #usuario = models.CharField('titulo', max_length=40)
-    # contrasena
-    #contra = models.CharField(u'contraseña', max_length=40)
-    # cursos asignados
-    cursos_a_cargo = models.ManyToManyField(Curso)
+
+class Preceptor(User):
+    cursos = models.ManyToManyField(Curso)
 
     class Meta:
-        #ordering = ['usuario']
-#        app_label = 'preceptores'
+        verbose_name = 'preceptor'
         verbose_name_plural = 'preceptores'
 
     # Similar a __str__, devuelve una descripcion mas amigable
     def __unicode__(self):
-        return ""
+        return self.username
 
     def tomar_asistencia(self):
-        
         return ""
 
     def observar(self, alumno, descripcion):
         self.observacion_set.create(descripcion=descripcion)
         return descripcion
+
+#Clase alumno:
+class Alumno(User):
+    # Atributos de la clase
+    usuario = models.CharField("Usuario", max_length=100)
+    reincorporacion = models.IntegerField("Reincorporacion", max_length=2, default=0)
+    dni = models.IntegerField("Dni", max_length=100, default=0)
+    
+    curso = models.ForeignKey(Curso)
+    
+    class Meta:
+        verbose_name = 'alumno'
+        verbose_name_plural = 'alumnos'
+
+    # Similar a __str__, devuelve una descripcion mas amigable
+    def __unicode__(self):
+        return self.username
+
+
+class Observacion (models.Model):
+    descripcion = models.CharField(max_length=200)
+    fecha = models.DateField(default=timezone.now())
+    # relacion con el preceptor
+    preceptor = models.ForeignKey(Preceptor)
+    alumno = models.ForeignKey(Alumno)
+
+    class Meta:
+        verbose_name_plural = 'observaciones'
+
+    # Similar a __str__, devuelve una descripcion mas amigable
+    def __unicode__(self):
+        return self.descripcion + self.fecha
+
+
+class Inasistencia (models.Model):
+    tipo = models.FloatField()
+    justificado = models.BooleanField()
+    fecha = models.DateField()
+
+    preceptor = models.ForeignKey(Preceptor)
+    alumno = models.ForeignKey(Alumno)
+
+    # Similar a __str__, devuelve una descripcion mas amigable
+    def __unicode__(self):
+        return self.fecha
+
 
