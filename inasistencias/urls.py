486470800f6cbfccc83cfa12279193a9422f4952
root <root@linux-lc.site>
2014-11-13 08:08:17 -0300

    Borrando todo..

--- inasistencias/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from RDFvevo import settings
-
-urlpatterns = patterns('',
-
-    url(r'^$', 'inasistencias.views.loguearse', name='login'),
-    url(r'^desloguearse/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
-    
-)
